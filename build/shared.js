bccc0993d726309bca8ada70bd16702f66325add
uaquax <uaquax@tutanota.com>
2024-04-30 20:01:25 +0800

    rm build

--- build/shared.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shared = void 0;
-exports.shared = {
-    waiting_message: undefined,
-    last_message: 0,
-    time_since_last_message: 0,
-};
